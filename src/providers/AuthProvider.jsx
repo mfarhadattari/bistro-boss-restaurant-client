bc6b19dfb54101f95b71c92ff90a3984938a7182
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-30 10:07:01 +0600

    createUser function define

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,12 +1,26 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "./../firebase/firebase.config";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const auth = getAuth(app);
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  /* --------------------------------------------------------
+  !------------------ CREATE USER --------------------------- */
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = {
+    user,
+    createUser,
+    loading,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
