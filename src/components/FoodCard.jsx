9d5f584b4442d4f3555c9a782ee6cef0b227e7e4
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-30 20:10:06 +0600

    save food on cart in database

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,7 +1,41 @@
+import { useContext } from "react";
 import AddToCartBtn from "./Buttons/AddToCartBtn";
+import { AuthContext } from "./../providers/AuthProvider";
+import Swal from "sweetalert2";
 
 const FoodCard = ({ item }) => {
+  const { user } = useContext(AuthContext);
   const { image, name, price, recipe } = item;
+
+  const handelAddToCart = (menuItem) => {
+    const { image, name, price, _id } = menuItem;
+    if (user && user.email) {
+      const cartItem = {
+        foodID: _id,
+        name,
+        image,
+        price,
+        email: user.email,
+      };
+      fetch("http://localhost:5000/carts", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(cartItem),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId || data.modifiedCount > 0) {
+            Swal.fire({
+              icon: "success",
+              title: "Successful",
+              text: "Successfully added to cart",
+            });
+          }
+        });
+    }
+  };
 
   return (
     <div className="card bg-[#F3F3F3] shadow-xl rounded-none">
@@ -16,7 +50,9 @@ const FoodCard = ({ item }) => {
       <div className="card-body">
         <h2 className="card-title w-fit mx-auto">{name}</h2>
         <p className="text-lg font-normal text-left text-[#737373]">{recipe}</p>
-        <AddToCartBtn>Add To Cart</AddToCartBtn>
+        <AddToCartBtn onClick={() => handelAddToCart(item)}>
+          Add To Cart
+        </AddToCartBtn>
       </div>
     </div>
   );
