f8e2e31ff048f1fb36f7dc6e2394609bcc3477a7
mfarhadattari <mfarhad.attari@gmail.com>
2023-05-30 09:47:36 +0600

    set up react hook form in sign up form and validate form and display error messsage

---
+++ src/components/Message/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+const ErrorMessage = ({ message }) => {
+  return <p className="text-red-600 text-lg my-1 w-[300px]">{message}</p>;
+};
+
+export default ErrorMessage;
--- src/pages/User/Register.jsx
+++ src/pages/User/Register.jsx
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom";
 import img from "../../assets/others/authentication.png";
+import { useForm } from "react-hook-form";
+import ErrorMessage from "../../components/Message/ErrorMessage";
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <section className="hero min-h-screen bg-authentication-img p-10">
-      <div className="hero-content flex-col lg:flex-row-reverse bg-authentication-img shadow-login py-5">
-        <div className="text-center lg:text-left">
-          <img src={img} alt="" />
+      <div className="w-[800px] hero-content flex-col lg:flex-row-reverse bg-authentication-img shadow-login py-5">
+        <div className="text-center w-full">
+          <img src={img} alt="" className="w-full" />
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm">
+        <div className="card  w-full">
           <h1 className="text-center text-[#151515] text-4xl font-semibold">
             Sign Up
           </h1>
-          <form className="card-body">
+          <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text text-[#444444] text-xl font-medium">
@@ -20,9 +32,14 @@ const Register = () => {
               </label>
               <input
                 type="text"
-                placeholder="Type Here"
+                placeholder="Enter Your Name"
+                name="name"
                 className="input input-bordered"
+                {...register("name", { required: true })}
               />
+              {errors.name && (
+                <ErrorMessage message="Name is required"></ErrorMessage>
+              )}
             </div>
 
             <div className="form-control">
@@ -33,9 +50,14 @@ const Register = () => {
               </label>
               <input
                 type="email"
-                placeholder="Type Here"
+                placeholder="Enter Your Email"
+                name="email"
                 className="input input-bordered"
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <ErrorMessage message="Email is required"></ErrorMessage>
+              )}
             </div>
 
             <div className="form-control">
@@ -47,15 +69,37 @@ const Register = () => {
               <input
                 type="password"
                 placeholder="Enter Your Password"
+                name="password"
                 className="input input-bordered"
+                {...register("password", {
+                  required: true,
+                  maxLength: 20,
+                  minLength: 6,
+                  pattern: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s])/,
+                })}
               />
+              {errors.password?.type === "required" && (
+                <ErrorMessage message="Password is required"></ErrorMessage>
+              )}
+              {errors.password?.type === "minLength" && (
+                <ErrorMessage message="Password must be 6 character"></ErrorMessage>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <ErrorMessage message="Password max 20 character"></ErrorMessage>
+              )}
+              {errors.password?.type === "pattern" && (
+                <ErrorMessage message="Password must have one uppercase, one lowercase, one number and one special character "></ErrorMessage>
+              )}
             </div>
 
             <div className="form-control mt-6">
               <button className="btn bg-[#D1A054] border-0">Sign Up</button>
             </div>
             <p className="text-[#D1A054] text-center mt-5">
-              Already registered? <Link className="underline underline-offset-4" to="/login">Go to Login</Link>
+              Already registered?{" "}
+              <Link className="underline underline-offset-4" to="/login">
+                Go to Login
+              </Link>
             </p>
           </form>
         </div>
