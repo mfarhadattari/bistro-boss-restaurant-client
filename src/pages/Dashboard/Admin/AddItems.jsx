239141672fdce6faa78048182fb1a83cf687131a
mfarhadattari <mfarhad.attari@gmail.com>
2023-06-02 11:00:36 +0600

    add item functionality added

--- src/pages/Dashboard/Admin/AddItems.jsx
+++ src/pages/Dashboard/Admin/AddItems.jsx
@@ -1,8 +1,58 @@
 import SetTitle from "./../../../components/SetTitle";
 import SectionHeader from "./../../../components/SectionHeader";
 import { FaUtensils } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import useSecureAxios from "../../../hooks/useSecureAxios";
+import ErrorMessage from "../../../components/Message/ErrorMessage";
+import useAuthContext from "../../../hooks/useAuthContext";
+import Swal from "sweetalert2";
 
 const AddItems = () => {
+  const { user } = useAuthContext();
+  const { axiosSecure } = useSecureAxios();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+
+    fetch(import.meta.env.VITE_IMG_HOSTING, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgResponse) => {
+        if (imgResponse.success) {
+          const imgURL = imgResponse.data.display_url;
+          const { name, category, price, recipe } = data;
+          const newItem = {
+            name,
+            category,
+            price: parseFloat(price),
+            recipe,
+            image: imgURL,
+          };
+          axiosSecure
+            .post(`/add-item?email=${user.email}`, newItem)
+            .then(({ data }) => {
+              if (data.insertedId) {
+                Swal.fire({
+                  icon: "success",
+                  title: "Added Item Successfully!",
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+              }
+            });
+        }
+      });
+  };
+
+
   return (
     <section className="container mx-auto">
       <SetTitle title="Add Item - Bistro Boss Restaurant"></SetTitle>
@@ -11,8 +61,8 @@ const AddItems = () => {
         subHeading="What's new?"
       ></SectionHeader>
       <div className="card bg-gray-100 w-4/5 mx-auto my-10">
-        <form className="card-body space-y-2">
-          <div className="form-control">
+        <form className="card-body space-y-2" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Recipe name*</span>
             </label>
@@ -20,7 +70,11 @@ const AddItems = () => {
               type="text"
               placeholder="Recipe name"
               className="input input-bordered"
+              {...register("name", { required: true })}
             />
+            {errors.name && (
+              <ErrorMessage message="Name is required"></ErrorMessage>
+            )}
           </div>
 
           <div className="flex flex-col md:flex-row gap-5">
@@ -28,23 +82,33 @@ const AddItems = () => {
               <label className="label">
                 <span className="label-text">Category*</span>
               </label>
-              <select className="select select-bordered w-full max-w-xs">
+              <select
+                className="select select-bordered w-full max-w-xs"
+                {...register("category", { required: true })}
+              >
                 <option>SALAD</option>
                 <option>PIZZA</option>
                 <option>DESSERT</option>
                 <option>SOUP</option>
                 <option>DRINK</option>
               </select>
+              {errors.category && (
+                <ErrorMessage message="Category is required"></ErrorMessage>
+              )}
             </div>
             <div className="form-control w-full">
               <label className="label">
                 <span className="label-text">Price*</span>
               </label>
               <input
+                {...register("price", { required: true })}
                 type="number"
                 placeholder="Price"
                 className="input input-bordered"
               />
+              {errors.price && (
+                <ErrorMessage message="Price is required"></ErrorMessage>
+              )}
             </div>
           </div>
           <div className="form-control w-full">
@@ -52,17 +116,25 @@ const AddItems = () => {
               <span className="label-text">Recipe Details*</span>
             </label>
             <textarea
+              {...register("recipe", { required: true })}
               className="textarea textarea-bordered"
               rows={6}
               placeholder="Recipe Details"
             ></textarea>
+            {errors.recipe && (
+              <ErrorMessage message="Recipe Details is required"></ErrorMessage>
+            )}
           </div>
 
           <div className="form-control w-full">
             <input
+              {...register("image", { required: true })}
               type="file"
               className="file-input w-full max-w-xs bg-gray-200"
             />
+            {errors.image && (
+              <ErrorMessage message="Image is required"></ErrorMessage>
+            )}
           </div>
 
           <div className="form-control mt-6">
